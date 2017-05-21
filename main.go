04b0b43d36ede101c245ed3abf133c851d7bd8f6
Alexandre Figueiredo <alexandre@Alexandres-MacBook-Pro.local>
2017-05-21 15:07:24 +0200

    Sorted out static file serving

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"net/http"
 )
@@ -11,29 +10,42 @@ var addr = flag.String("addr", ":8080", "http service address")
 
 func main() {
 	flag.Parse()
+	fs := http.FileServer(http.Dir("static"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
 	hub := newHub()
 	go hub.run()
-	http.Handle("/", http.FileServer(http.Dir("./public")))
+	http.HandleFunc("/", serveHome)
+	http.HandleFunc("/control", serveControl)
 	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
 		serveWs(hub, w, r)
 	})
 
-	fmt.Printf("Listening on port %s", *addr)
+	log.Printf("Listening on port %s", *addr)
 	err := http.ListenAndServe(*addr, nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
 }
 
-// func serveHome(w http.ResponseWriter, r *http.Request) {
-// 	log.Println(r.URL)
-// 	if r.URL.Path != "/" {
-// 		http.Error(w, "Not found", 404)
-// 		return
-// 	}
-// 	if r.Method != "GET" {
-// 		http.Error(w, "Method not allowed", 405)
-// 		return
-// 	}
-// 	http.ServeFile(w, r, "home.html")
-// }
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	log.Printf("serveHome called: %s", r.URL)
+	if r.URL.Path != "/" {
+		http.Error(w, "Not found", 404)
+		log.Printf("No Found: %s", r.URL)
+		return
+	}
+	if r.Method != "GET" {
+		http.Error(w, "Method not allowed", 405)
+		log.Printf("Method not allowed: %s", r.URL)
+		return
+	}
+
+	log.Printf("Serving home: %s", r.URL)
+	http.ServeFile(w, r, "./html/home.html")
+}
+
+func serveControl(w http.ResponseWriter, r *http.Request) {
+	log.Println("serveControl called")
+	http.ServeFile(w, r, "./html/control.html")
+	return
+}
